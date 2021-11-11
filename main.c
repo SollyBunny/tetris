42afe6fb56c718da40fa77ce6e721e7f67605f90
Solly Bunny <76408142+SollyBunny@users.noreply.github.com>
2021-11-11 10:50:01 +0000

    i made it fall ;D

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+// Key codes
+#define BREAK 3
+
+#define KEYW 119
+#define KEYA 97
+#define KEYS 115
+#define KEYD 100
 
 unsigned int gridx, gridy, gridsize;
 unsigned int *grid;
@@ -46,31 +56,38 @@ unsigned int tiles[][3 * 4] = {
 unsigned int currenttile;
 unsigned int currentpos;
 
+// Used instead of getchar, it will return 0 if theres no character pressed, otherwise returned pressed
+char _waiting;
+char sgetchar() {
+	ioctl(0, FIONREAD, &_waiting);
+	if (_waiting == 0) return 0;
+	return getchar();
+}
+
 void print() {
 
 	unsigned int x, y;
 
 	for (y = 0; y < gridy; ++y) {
 		for (x = 0; x < gridx; ++x) {
-			putchar('[');
-			putchar(']');
+			putchar(' ');
+			putchar(' ');
 		}
 		putchar('\n');
 	}
 
-	printf("\x1b[%u;%uH", currentpos / gridy, currentpos % gridy);
-	goto printstartloop;
+	printf("\x1b[48;5;255m\x1b[%u;%uH", currentpos / gridx, (currentpos % gridx) * 2);
 	for (x = 0; x < 3 * 4; ++x) {
-		if (x % 3 == 0) {
-			printf("\x1b[%u;%uH", (currentpos / gridy) + (x / 3) + 1, currentpos % gridy);
+		if (x % 3 == 0 && x != 0) {
+			printf("\x1b[1B\x1b[6D");
 		}
-		printstartloop:
 		if (tiles[currenttile][x] == 1) {
 			putchar('#'); putchar('#');
 		} else {
-			putchar(' ');putchar(' ');
+			printf("\x1b[2C");
 		}
 	}
+	printf("\x1b[48;5;0m");
 
 }
 
@@ -88,11 +105,38 @@ int main(void) {
 	currenttile = 0;
 	currentpos = gridx / 2;
 
-	printf("\x1b[2J\x1b[H");
+	printf("\x1b[?25l");
 
-	print();
+	unsigned int count = 0;
+	char c;
+	while ((c = sgetchar()) != BREAK) {
 
-	
+		switch (c) {
+			case KEYS:
+				currentpos += gridx;
+				break;
+			case KEYA:
+				if (currentpos % gridx == 0) 
+					break;
+				currentpos -= 1;
+				break;
+			case KEYD:
+				if (currentpos % gridx == gridx) 
+					break;
+				currentpos += 1;
+				break;
+		}
+
+		printf("\x1b[2J\x1b[H");
+
+		print();
+		fflush(stdout);
+		// printf("%d\n", count);
+
+		usleep(200000);
+		
+
+	}
 
 	return 0;
 }

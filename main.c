ea261d294349145af3179edd8a50c625222a7e06
SollyBunny <sollybunn@gmail.com>
2021-12-07 21:30:14 +0000

    irdk

--- main.c
+++ main.c
@@ -295,36 +295,44 @@ struct Block tiles[][4] = {
 	}
 };
 
+unsigned int tilescolor[] = {
+	226, //block
+	87, //bar
+	46, //s
+	45, //z
+	208,//l
+	21, //j
+	93 //t
+};
+
 unsigned int currenttile;
 unsigned int currentpos;
 unsigned int currentdir;
+
+struct termios term, restore;
+void sexit() {
+	printf("\x1b[?25h\x1b[0m\x1b[%u;0H", gridy + 1); //show cursor, goto end position, assume color has been reset
+	tcsetattr(0, TCSANOW, &restore); // restore terminal state
+	exit(0);
+}
 
 void print() {
 
 	unsigned int x, y, t;
 
 	printf("\x1b[2J\x1b[H\x1b[48;5;1m");
-	// for (y = 0; y < gridy; ++y) {
-	// 	for (x = 0; x < gridx; ++x) {
-	// 		if (grid[x])
-	// 			printf("\x1b[2C");
-	// 		else
-	// 			printf("AA");
-	// 		// putchar(' ');
-	// 		// putchar(' ');
-	// 	}
-	// 	putchar('\n');
-	// }
+
+	// TODO clear with color, then empty out screen ONCE
+
 	for (t = 0; t < gridsize; ++t) {
 
-		if (grid[t] == 1) {
-			printf("\x1b[48;5;240m");
+		if (grid[t] != 0) { 
 			do { // continue rendering tiles untill theres no more (or theres a newline)
-				putchar(' '); putchar(' '); 
+				printf("\x1b[48;5;%dm  ", tilescolor[grid[t] - 1]);
 				if (t % gridx == gridx - 1) // TODO messy :P // same as line ~334
 					putchar('\n');	
 				++t;
-			} while ((grid[t] == 1));
+			} while (t < gridsize && (grid[t] != 0));
 			printf("\x1b[48;5;1m");
 			--t;
 		} else {
@@ -337,31 +345,42 @@ void print() {
 
 	}
 
+	//print current tile falling
 	y = (currentpos % (gridx + 3));
 	// if (y == 0)
 		// printf("%d\x1b[48;5;255m\x1b[%u;%uH", y, currentpos / gridx, 0);
 	// else
+	printf("\x1b[48;5;%dm", tilescolor[currenttile]);
 	if (y - 3 > gridx) { // (t < 0) // compatibility with terminals with line wrap when you goto position -1
-		printf("\x1b[48;5;240m\x1b[%u;0H", (currentpos / (gridx + 3)));
+		printf("\x1b[%u;0H", (currentpos / (gridx + 3) - 3));
 	} else {
-		printf("\x1b[48;5;240m\x1b[%u;%uH", (currentpos / (gridx + 3)), y * 2 - 5);
+		printf("\x1b[%u;%uH", (currentpos / (gridx + 3) - 3), y * 2 - 5);
 	}
 
-	for (x = 0; x < 4 * 4; ++x) {
+	x = currentpos / (gridx + 3);
+	if (x < 4) {
+		x = (3 - x) * 4;
+	} else x = 0;
+
+	t = x % 4;
+	goto printloopstart;
+	for (; x < 4 * 4; ++x) {
 
 		t = x % 4;
-		
-		if (t == 0 && x != 0) {
+
+		if (t == 0) {
 			printf("\x1b[8D\x1b[1B");
 		}
-		
+
+		printloopstart:
+
 		if (y < 3 && t < (3 - y)) continue;  // dont render stuff off the left side
 		
 		if (tiles[currenttile][currentdir].data[x] == 1) {
 			putchar(' '); putchar(' ');
 		} else {
 			printf("\x1b[2C");
-			//printf("__"); // debug
+			// /printf("__"); // debug
 		}
 		
 	}
@@ -370,11 +389,99 @@ void print() {
 
 }
 
+void tilereset() {
+	// if (currenttile == 6) currenttile = 0;
+	// else currenttile++;
+	currenttile = rand() % 7;
+	currentpos = gridx / 2;
+	currentdir = 0;
+	print();
+}
+
+void movedown() {
+
+	unsigned int i;
+	unsigned int x = (currentpos % (gridx + 3));
+	unsigned int y = currentpos / (gridx + 3) + 1;
+
+	// calculate collision of bottom
+	if (
+		y > gridy - 3 &&
+		y > gridy + tiles[currenttile][currentdir].bounding[3]
+	) goto movecollisionignorebottom;
+
+	// skip out top chunk (seg fault prot)
+	if (y < 7) i = (7 - y) * 4;
+	else       i = 0; // probs redundant
+	
+	for (; i < 4 * 4; ++i) {
+
+		if (tiles[currenttile][currentdir].data[i]) {
+
+		// i = iterator for tile information
+			if (grid[
+				( // y value
+					y
+					+ (i / 4)
+					- 4
+				) * gridx
+				+ x
+				+ (i % 4)
+				- 3
+			]) goto movecollision;
+
+		}
+
+	}
+
+	currentpos += gridx;
+	currentpos += 3;
+
+	print();
+	
+	return;
+
+	movecollision:
+
+		if (y < 5) {
+			if (currenttile == 6) currenttile = 0;
+			else currenttile++;
+			currentpos = gridx * 1.5 + 3;
+			currentdir = 0;
+			print();
+			sexit();
+		}
+
+	movecollisionignorebottom:
+
+		for (i = 0; i < 4 * 4; ++i) {
+			if (tiles[currenttile][currentdir].data[i]) {
+				grid[
+					( // y value
+						y
+						+ (i / 4)
+						- 5
+					) * gridx
+					+ x
+					+ (i % 4)
+					- 3
+				] = currenttile + 1;
+			}
+		}
+
+		tilereset();
+
+	print();
+	
+}
+
 void move(unsigned int newpos) {
 
 	unsigned int i;
 	unsigned int x = (newpos % (gridx + 3));
+	unsigned int y = y = currentpos / (gridx + 3);
 
+	// calc collsion left/right
 	if (x > gridx / 2) {
 		while (x > tiles[currenttile][currentdir].bounding[1] + gridx - 1) {
 			newpos--;
@@ -387,76 +494,59 @@ void move(unsigned int newpos) {
 		}
 	}
 
-	// calculate collision
-		if (newpos / (gridx + 3) > gridy - 3 - tiles[currenttile][currentdir].bounding[3]) goto movecollision;
+	// skip out top chunk (seg fault prot)
+	if (y < 4) i = ((4 - y) + 1) * 4;
+	else       i = 0; // probs redundant
 	
-		for (i = 0; i < )
+	for (; i < 4 * 4; ++i) {
+		if (tiles[currenttile][currentdir].data[i]) {
+
+			if (grid[
+				( // y value
+					y
+					+ (i / 4)
+					- 3
+				) * gridx
+				+ x
+				+ (i % 4)
+				- 3
+			]) return;
+		}
+	}
 
 	currentpos = newpos;
-
 	
-	// debug
-	// while (currentpos > gridsize)
-		// currentpos -= 1;
-		// currentpos -= gridsize - 2;
-
 	print();	
-
-	return;
-
-	movecollision:
-
-		// i = iterator for tile information
-		for (i = 0; i < 4 * 4; ++i) {
-			if (tiles[currenttile][currentdir].data[i]) {
-				grid[
-					( // y value
-						(currentpos / (gridx + 3)) +
-						(i / 4) - 
-						1
-					) * gridx
-					+ x
-					+ (i % 4)
-					- 3
-				] = 1;
-			}
-		}
-
-		currenttile = 2;
-		currentpos = gridx / 2;
-		currentdir = 0;
-
-		print();
-		
-	return;
 
 }
 
 void input(void) {
 	while (1) {
-		move(currentpos + gridx + 3);
+		movedown(currentpos + gridx + 3);
 		usleep(200000);
 	}
 }
 
 int main(void) {
 
+	time_t _t;
+	srand(time(&_t));
+
 	gridx = 10;
 	gridy = 21;
 	gridsize = gridx * gridy;
 
 	grid = malloc(gridsize * sizeof(unsigned int));
-	for (unsigned int *m = grid; m; m++) {
-		*m = 0;
-	}
+	for (unsigned int i = 0; i < gridsize; ++i) {
+		grid[i] = 0;
+	} // idk y but malloc is giving me all 0's even tho its uninitialized idk
 
-	currenttile = 1;
-	currentpos = gridx / 2;
-	currentdir = 0;
+	// currenttile = 1;
+	// currentpos = gridx / 2;
+	// currentdir = 0;
+	tilereset();
 
 	// set terminal settins (dont echo characters / queue stdin / ignore ctrl-c )
-	//system("/bin/stty -echo -ixon -icanon time 5 min 1 intr undef");
-	struct termios term, restore;
 	tcgetattr(0, &term);
 	tcgetattr(0, &restore); // backup the original terminal state to restore later
 	term.c_lflag &= ~(ICANON|ECHO|~ISIG);
@@ -472,7 +562,7 @@ int main(void) {
 		// printf("%d, %d|", currentpos, c); // debug
 		switch (c) {
 			case KEYS:
-				move(currentpos + gridx + 3);
+				movedown();
 				break;
 			case KEYA:
 				// if (currentpos % gridx == 1)
@@ -498,8 +588,6 @@ int main(void) {
 
 	}
 
-	fputs("\x1b[?25h\x1b[0m\x1b[2J\x1b[H", stdout); // clear, show cursor, goto home position, assume color has been reset
-	tcsetattr(0, TCSANOW, &restore); // restore terminal state
-
-	return 0;
+	sexit();
+	
 }
